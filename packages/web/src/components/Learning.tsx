66f7eb0f51b39ff28b9bda8835ce1172d7e45c89
Ramzan Chasygov <theliketurbo@gmail.com>
2019-10-15 21:47:50 +0300

    Added video component

--- packages/web/src/components/Learning.tsx
+++ packages/web/src/components/Learning.tsx
@@ -1,9 +1,36 @@
 import React from "react"
 import Youtube from "react-youtube"
+import styled from "styled-components"
+
+const Container = styled.section`
+  padding: 0 calc(50% - ${props => props.theme.spacing["112"]});
+  border: ${props => props.theme.spacing["4"]} solid transparent;
+`
+
+const Title = styled.h1`
+  ${tw`text-black text-center mb-4 text-2xl font-bold`}
+`
+
+const Video = styled(Youtube)`
+  width: 100%;
+  height: 90vh;
+
+  @media (max-width: ${props => props.theme.screens.lg}) {
+    height: 70vh;
+  }
+
+  @media (max-width: ${props => props.theme.screens.md}) {
+    height: 60vh;
+  }
+
+  @media (max-width: ${props => props.theme.screens.sm}) {
+    height: 50vh;
+  }
+`
 
 export default () => (
-  <section>
-    <h1>Обучение по работе с таксометром</h1>
-    <Youtube videoId="Qz0xUI-PXV0" />
-  </section>
+  <Container>
+    <Title>Обучение по работе с таксометром</Title>
+    <Video videoId="Qz0xUI-PXV0" />
+  </Container>
 )
