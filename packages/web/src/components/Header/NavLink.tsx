95c3288636b73e4ebedacc70136c40953a51cf5e
Ramzan Chasygov <theliketurbo@gmail.com>
2019-10-26 10:08:09 +0300

    Fixed bug with NavLink on small screen

--- packages/web/src/components/Header/NavLink.tsx
+++ packages/web/src/components/Header/NavLink.tsx
@@ -5,7 +5,7 @@ const Item = styled.li`
   ${tw`mx-1`}
 
   > a {
-    ${tw`px-2 pb-4 text-black hover:text-red border-b-2 border-transparent hover:border-red`}
+    ${tw`px-2 pb-4 text-black hover:text-red border-b-0 md:border-b-2 border-transparent hover:border-red`}
 
     transition: all 0.15s ease;
   }
