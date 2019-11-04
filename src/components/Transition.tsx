e3753d012a558e3ee77195a9344448f1de63d56d
Ramzan Chasygov <theliketurbo@gmail.com>
2019-11-04 10:08:15 +0300

    Added styled-components to trnasition

--- src/components/Transition.tsx
+++ src/components/Transition.tsx
@@ -1,44 +1,37 @@
 import React, { ReactNode } from "react"
 import { Transition, TransitionGroup } from "react-transition-group"
+import { TransitionStatus } from "react-transition-group/Transition"
+import styled from "styled-components"
 
 import useRouter from "../hooks/useRouter"
 
-const timeout = 150
-const getTransitionStyles = {
-  entering: {
-    opacity: 0,
-  },
-  entered: {
-    transition: `opacity ${timeout}ms ease`,
-    opacity: 1,
-  },
-  exiting: {
-    transition: `opacity ${timeout}ms ease`,
-    opacity: 0,
-  },
-} as any
+const Animation = styled.div<{
+  state: TransitionStatus
+}>`
+  transition: opacity 0.15s ease;
+  opacity: ${({ state }) => {
+    switch (state) {
+      case "entering":
+        return 0
+      case "entered":
+        return 1
+      case "exiting":
+        return 0
+      case "exited":
+        return 1
+      default:
+        return 1
+    }
+  }};
+`
 
 export default ({ children }: { children: ReactNode }) => {
   const { location } = useRouter()
 
   return (
     <TransitionGroup>
-      <Transition
-        key={location.pathname}
-        timeout={{
-          enter: timeout,
-          exit: timeout,
-        }}
-      >
-        {status => (
-          <div
-            style={{
-              ...getTransitionStyles[status],
-            }}
-          >
-            {children}
-          </div>
-        )}
+      <Transition key={location.pathname} timeout={150}>
+        {state => <Animation {...{ state }}>{children}</Animation>}
       </Transition>
     </TransitionGroup>
   )
